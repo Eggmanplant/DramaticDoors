db3f9b8a25809ecda5942f8cec91b0baa441b30c
Kitteh6660 <Kitteh6660@users.noreply.github.com>
2022-06-07 17:32:31 -0400

    Fabric 1.18.2 - Mod Update 1.1.1
    Fixes incompatibility with Lithium, and fixes LVT by eliminating local capture.

--- src/main/java/com/fizzware/dramaticdoors/mixin/LandPathNodeMakerMixin.java
+++ src/main/java/com/fizzware/dramaticdoors/mixin/LandPathNodeMakerMixin.java
@@ -4,12 +4,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import com.fizzware.dramaticdoors.blocks.TallDoorBlock;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Material;
 import net.minecraft.entity.ai.pathing.LandPathNodeMaker;
 import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.util.math.BlockPos;
@@ -18,9 +15,10 @@ import net.minecraft.world.BlockView;
 @Mixin(LandPathNodeMaker.class)
 public class LandPathNodeMakerMixin
 {
-	@Inject(at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILSOFT, method = "getCommonNodeType(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/entity/ai/pathing/PathNodeType;", cancellable = true)
-	private static void injectDoorType(BlockView world, BlockPos pos, CallbackInfoReturnable<PathNodeType> callback, BlockState blockState, Block block, Material material) {
-		if (TallDoorBlock.isWoodenDoor(blockState) && !world.getBlockState(pos).get(TallDoorBlock.OPEN).booleanValue()) {
+	@Inject(at = @At("RETURN"), method = "getCommonNodeType(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/entity/ai/pathing/PathNodeType;", cancellable = true)
+	private static void injectDoorType(BlockView world, BlockPos pos, CallbackInfoReturnable<PathNodeType> callback) {
+		BlockState blockStateDDCheck = world.getBlockState(pos);
+		if (TallDoorBlock.isWoodenDoor(blockStateDDCheck) && !blockStateDDCheck.get(TallDoorBlock.OPEN).booleanValue()) {
             callback.setReturnValue(PathNodeType.DOOR_WOOD_CLOSED);
         }
 	}
